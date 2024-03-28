bb0d3fe65beeb932b0f06ada197fcd90bbf2963c
Swann <swann.rouanet@gmail.com>
2024-03-28 18:22:18 +0100

    Add basic fabric template

---
+++ src/main/java/net/hezaerd/terraccessories/TerraccessoriesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hezaerd.terraccessories;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TerraccessoriesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
