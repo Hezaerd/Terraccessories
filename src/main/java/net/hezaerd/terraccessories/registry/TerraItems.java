1a2e0bf4a3baa08607248c7ff874fc3e9771cd39
Angel-2180 <a.gimer@student.isartdigital.com>
2024-03-31 02:10:12 -0400

    Add DemonConch

--- src/main/java/net/hezaerd/terraccessories/registry/TerraItems.java
+++ src/main/java/net/hezaerd/terraccessories/registry/TerraItems.java
@@ -2,6 +2,7 @@ package net.hezaerd.terraccessories.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.hezaerd.terraccessories.items.DemonConch;
 import net.hezaerd.terraccessories.items.InfiniteLavaBucket;
 import net.hezaerd.terraccessories.items.InfiniteWaterBucket;
 import net.hezaerd.terraccessories.items.MagicMirror;
@@ -17,6 +18,8 @@ public class TerraItems {
     public static final InfiniteWaterBucket INFINITE_WATER_BUCKET = new InfiniteWaterBucket(new FabricItemSettings());
     public static final InfiniteLavaBucket INFINITE_LAVA_BUCKET = new InfiniteLavaBucket(new FabricItemSettings());
 
+    public  static final DemonConch DEMON_CONCH = new DemonConch(new FabricItemSettings());
+
     private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
             .icon(() -> new ItemStack(INFINITE_WATER_BUCKET))
             .displayName(Text.translatable("itemGroup.terraccessories"))
@@ -24,6 +27,7 @@ public class TerraItems {
                 entries.add(INFINITE_WATER_BUCKET);
                 entries.add(INFINITE_LAVA_BUCKET);
                 entries.add(MAGIC_MIRROR);
+                entries.add(DEMON_CONCH);
             }).build();
 
     public static void register() {
@@ -32,5 +36,6 @@ public class TerraItems {
         Registry.register(Registries.ITEM, new Identifier("terraccessories", "magic_mirror"), MAGIC_MIRROR);
         Registry.register(Registries.ITEM, new Identifier("terraccessories", "infinite_water_bucket"), INFINITE_WATER_BUCKET);
         Registry.register(Registries.ITEM, new Identifier("terraccessories", "infinite_lava_bucket"), INFINITE_LAVA_BUCKET);
+        Registry.register(Registries.ITEM, new Identifier("terraccessories", "demon_conch"), DEMON_CONCH);
     }
 }
