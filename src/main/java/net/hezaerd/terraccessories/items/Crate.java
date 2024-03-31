f4e305ba1c6603980d3ff55bfe742afbc0365251
RaphaelDaubelcour <daubelcour63@gmail.com>
2024-03-31 16:46:57 -0400

    Update Crate Textures

--- src/main/java/net/hezaerd/terraccessories/items/Crate.java
+++ src/main/java/net/hezaerd/terraccessories/items/Crate.java
@@ -7,12 +7,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
 public class Crate extends Item {
-    private static final int cooldown = 2;
-
     public Crate(Settings settings) {
         super(settings);
     }
@@ -21,7 +20,6 @@ public class Crate extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         user.playSound(SoundEvents.BLOCK_WOOD_BREAK, 1.0F, 1.0F);
         user.getStackInHand(hand).decrement(1);
-        user.getItemCooldownManager().set(this, cooldown);
 
         if (Random.create().nextBetween(0, 100) == 0)
             user.getInventory().insertStack(new ItemStack(TerraItems.MAGIC_MIRROR, 1));
