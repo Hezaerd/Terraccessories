e19dee0efec16dee5095b659dd9b5ed9ba65702c
RaphaelDaubelcour <daubelcour63@gmail.com>
2024-03-31 01:28:35 -0400

    Add Infinite Lava Bucket

--- src/main/java/net/hezaerd/terraccessories/items/InfiniteLavaBucket.java
+++ src/main/java/net/hezaerd/terraccessories/items/InfiniteLavaBucket.java
@@ -1,24 +1,24 @@
 package net.hezaerd.terraccessories.items;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.UseAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
-import net.minecraft.util.Formatting;
-import net.minecraft.text.Text;
-import net.minecraft.item.ItemStack;
-import net.minecraft.client.item.TooltipContext;
 
 import java.util.List;
 
-public class InfiniteWaterBucket extends Item {
-    public InfiniteWaterBucket(Settings settings) {
+public class InfiniteLavaBucket extends Item {
+    public InfiniteLavaBucket(Settings settings) {
         super(settings.maxCount(1));
     }
 
@@ -27,14 +27,14 @@ public class InfiniteWaterBucket extends Item {
         World world = context.getWorld();
         BlockPos blockPos = context.getBlockPos();
 
-        world.playSound(null, blockPos, SoundEvents.ITEM_BUCKET_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
-        world.setBlockState(blockPos.offset(context.getSide()), Blocks.WATER.getDefaultState());
+        world.playSound(null, blockPos, SoundEvents.ITEM_BUCKET_EMPTY_LAVA, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        world.setBlockState(blockPos.offset(context.getSide()), Blocks.LAVA.getDefaultState());
 
         return ActionResult.PASS;
     }
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-        tooltip.add(Text.translatable("item.terraccessories.infinite_water_bucket.tooltip").formatted(Formatting.GOLD));
+        tooltip.add(Text.translatable("item.terraccessories.infinite_lava_bucket.tooltip").formatted(Formatting.GOLD));
     }
 }
--- src/main/java/net/hezaerd/terraccessories/registry/TerraItems.java
+++ src/main/java/net/hezaerd/terraccessories/registry/TerraItems.java
@@ -2,6 +2,7 @@ package net.hezaerd.terraccessories.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.hezaerd.terraccessories.items.InfiniteLavaBucket;
 import net.hezaerd.terraccessories.items.InfiniteWaterBucket;
 import net.hezaerd.terraccessories.items.MagicMirror;
 import net.minecraft.item.ItemGroup;
@@ -14,12 +15,14 @@ import net.minecraft.util.Identifier;
 public class TerraItems {
     public static final MagicMirror MAGIC_MIRROR = new MagicMirror(new FabricItemSettings());
     public static final InfiniteWaterBucket INFINITE_WATER_BUCKET = new InfiniteWaterBucket(new FabricItemSettings());
+    public static final InfiniteLavaBucket INFINITE_LAVA_BUCKET = new InfiniteLavaBucket(new FabricItemSettings());
 
     private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
             .icon(() -> new ItemStack(INFINITE_WATER_BUCKET))
             .displayName(Text.translatable("itemGroup.terraccessories"))
             .entries((context, entries) -> {
                 entries.add(INFINITE_WATER_BUCKET);
+                entries.add(INFINITE_LAVA_BUCKET);
                 entries.add(MAGIC_MIRROR);
             }).build();
 
@@ -28,5 +31,6 @@ public class TerraItems {
 
         Registry.register(Registries.ITEM, new Identifier("terraccessories", "magic_mirror"), MAGIC_MIRROR);
         Registry.register(Registries.ITEM, new Identifier("terraccessories", "infinite_water_bucket"), INFINITE_WATER_BUCKET);
+        Registry.register(Registries.ITEM, new Identifier("terraccessories", "infinite_lava_bucket"), INFINITE_LAVA_BUCKET);
     }
 }
