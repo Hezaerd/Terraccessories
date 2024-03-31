5b60371c40df3e86e28defb87fe2eeb6b31dffda
Swann <swann.rouanet@gmail.com>
2024-03-31 22:46:52 +0200

    Add owo-lib and basic config placeholder

--- src/main/java/net/hezaerd/terraccessories/Terraccessories.java
+++ src/main/java/net/hezaerd/terraccessories/Terraccessories.java
@@ -10,9 +10,13 @@ public class Terraccessories implements ModInitializer {
 	public static final String MOD_ID = "terraccessories";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final net.hezaerd.terraccessories.TerraccessoriesConfig CONFIG = net.hezaerd.terraccessories.TerraccessoriesConfig.createAndLoad();
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
+
+
 
 		TerraItems.register();
 	}
