695675b3eaa4325fe55360afcd5f9b7153da8cf6
Swann <swann.rouanet@gmail.com>
2024-03-31 01:10:24 +0100

    Add Trinkets deps

---
+++ src/main/java/net/hezaerd/terraccessories/TerraccessoriesClient.java
@@ -0,0 +1,10 @@
+package net.hezaerd.terraccessories;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TerraccessoriesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hello Fabric world! (Client)");
+    }
+}
