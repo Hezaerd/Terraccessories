e7d0a4a7f5b15e7c9668fe3dd578ab37b39364cc
Swann <swann.rouanet@gmail.com>
2024-04-01 19:59:37 +0200

    Refactor how mirrors are registered

--- src/main/java/net/hezaerd/terraccessories/registry/ItemsInit.java
+++ src/main/java/net/hezaerd/terraccessories/registry/ItemsInit.java
@@ -2,11 +2,10 @@ package net.hezaerd.terraccessories.registry;
 
 import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
 import net.hezaerd.terraccessories.items.DemonConch;
+import net.hezaerd.terraccessories.items.MagicMirror;
 import net.hezaerd.terraccessories.items.RandomTeleportationPotion;
 import net.hezaerd.terraccessories.items.bucket.BottomlessLavaBucket;
 import net.hezaerd.terraccessories.items.bucket.BottomlessWaterBucket;
-import net.hezaerd.terraccessories.items.mirrors.IceMirror;
-import net.hezaerd.terraccessories.items.mirrors.MagicMirror;
 import net.hezaerd.terraccessories.items.trinkets.Aglet;
 import net.hezaerd.terraccessories.items.trinkets.AkletOfTheWind;
 import net.hezaerd.terraccessories.items.trinkets.HermesBoots;
@@ -15,7 +14,7 @@ import net.minecraft.item.Item;
 public class ItemsInit implements ItemRegistryContainer {
 
     public static final Item MAGIC_MIRROR = new MagicMirror();
-    public static final Item ICE_MIRROR = new IceMirror();
+    public static final Item ICE_MIRROR = new MagicMirror();
     public static final Item DEMON_CONCH = new DemonConch();
     public static final Item BOTTOMLESS_WATER_BUCKET = new BottomlessWaterBucket();
     public static final Item BOTTOMLESS_LAVA_BUCKET = new BottomlessLavaBucket();
