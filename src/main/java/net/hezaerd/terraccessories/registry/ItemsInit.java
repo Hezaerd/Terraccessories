7e5a3566db7787c7182269cdb847113eae122db4
Swann <swann.rouanet@gmail.com>
2024-04-01 16:49:55 +0200

    Add aklet of the wind

--- src/main/java/net/hezaerd/terraccessories/registry/ItemsInit.java
+++ src/main/java/net/hezaerd/terraccessories/registry/ItemsInit.java
@@ -9,6 +9,7 @@ import net.hezaerd.terraccessories.items.bucket.BottomlessWaterBucket;
 import net.hezaerd.terraccessories.items.mirrors.IceMirror;
 import net.hezaerd.terraccessories.items.mirrors.MagicMirror;
 import net.hezaerd.terraccessories.items.trinkets.Aglet;
+import net.hezaerd.terraccessories.items.trinkets.AkletOfTheWind;
 import net.minecraft.item.Item;
 
 public class ItemsInit implements ItemRegistryContainer {
@@ -20,4 +21,5 @@ public class ItemsInit implements ItemRegistryContainer {
     public static final Item BOTTOMLESS_LAVA_BUCKET = new BottomlessLavaBucket();
     public static final Item RANDOM_TELEPORTATION_POTION = new RandomTeleportationPotion();
     public static final Item AGLET = new Aglet();
+    public static final Item AKLET_OF_THE_WIND = new AkletOfTheWind();
 }
