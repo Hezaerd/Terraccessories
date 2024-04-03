1f51d0462af485329fdceef301cd4d02315db0d0
Swann <swann.rouanet@gmail.com>
2024-04-04 00:34:17 +0200

    Create TerraaccessoriesDataGen package

---
+++ src/main/java/net/hezaerd/terraccessories/datagen/TerraccessoriesDataGenerator.java
@@ -0,0 +1,12 @@
+package net.hezaerd.terraccessories.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TerraccessoriesDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
