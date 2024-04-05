963f1ebd48347bd61d581ee0ef15d303ef9f0852
Angel-2180 <a.gimer@student.isartdigital.com>
2024-04-05 13:29:15 -0400

    Update BootsUtils.java

--- src/main/java/net/hezaerd/terraccessories/utils/BootsUtils.java
+++ src/main/java/net/hezaerd/terraccessories/utils/BootsUtils.java
@@ -13,13 +13,44 @@ public class BootsUtils {
     {}
 
     public static  void  walkOnLava(PlayerEntity player) {
-        if (player.isInLava()) {
-            ShapeContext shapeContext = ShapeContext.of(player);
-            if (shapeContext.isAbove(FluidBlock.COLLISION_SHAPE, player.getBlockPos(), true) && !player.getWorld().getFluidState(player.getBlockPos().up()).isIn(FluidTags.LAVA)) {
-                player.setOnGround(true);
-            } else {
-                player.setVelocity(player.getVelocity().multiply(0.5).add(0.0, 0.05, 0.0));
+        if (player == null) return;
+        if (player.isSneaking() || player.isOnGround()) {
+            player.setNoGravity(false);
+            return;
+        }
+
+        if (player.isTouchingWater()) {}
+        BlockState blockStateDown = player.getWorld().getBlockState(player.getBlockPos().down());
+        BlockState blockStateFoot = player.getWorld().getBlockState(player.getBlockPos());
+
+        //Get the distance to the surface of the water
+        double distanceToSurface = (Math.floor(player.getY())) - player.getY();
+
+//        if (distanceToSurface > MAX_DISTANCE_SURFACE) {
+//            player.setNoGravity(false);
+//            return;
+//        }
+
+        if (blockStateDown.getBlock() == Blocks.LAVA && blockStateFoot.getBlock() != Blocks.LAVA) {
+            player.setNoGravity(true);
+
+            //Clamp the distance to the surface to a maximum of 0.04
+            distanceToSurface = Math.min(0.04, distanceToSurface);
+
+            //Set the player's velocity to the distance to the surface if it is greater than the player's current velocity
+            player.setVelocity(player.getVelocity().x, Math.max(distanceToSurface, player.getVelocity().y), player.getVelocity().z);
+            MinecraftClient client = MinecraftClient.getInstance();
+            if (client != null && client.player != null  && client.player.input != null)
+            {
+                client.player.setSprinting(!player.isSprinting() && client.player.input.pressingForward);
+
+                if (client.player.input.jumping){
+                    player.jump();
+                }
             }
+        }
+        else {
+            player.setNoGravity(false);
         }
 
     }
