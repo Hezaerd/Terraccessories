cf957c600de269f84a974b32ec4b594b014dcdf1
Swann <swann.rouanet@gmail.com>
2024-04-07 01:06:32 +0200

    Add repair to Staff of Regrowth

--- src/main/java/net/hezaerd/terraccessories/item/StaffOfRegrowth.java
+++ src/main/java/net/hezaerd/terraccessories/item/StaffOfRegrowth.java
@@ -11,12 +11,15 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
@@ -25,7 +28,7 @@ import java.util.List;
 
 public class StaffOfRegrowth extends Item {
     public StaffOfRegrowth() {
-        super(new OwoItemSettings().group(Terraccessories.TERRACCESSORIES_GROUP).maxCount(1).maxDamage(640));
+        super(new OwoItemSettings().group(Terraccessories.TERRACCESSORIES_GROUP).maxCount(1).maxDamage(320));
     }
 
     /* Tooltip */
@@ -34,7 +37,56 @@ public class StaffOfRegrowth extends Item {
         tooltip.add(Text.translatable("item.terraccessories.staff_of_regrowth.tooltip").formatted(Formatting.DARK_GREEN));
     }
 
+    /* Stop mining */
+    @Override
+    public boolean canMine(BlockState state, World world, BlockPos pos, PlayerEntity miner) {
+        return false;
+    }
 
+    /* Use */
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (user.isSneaking()) {
+
+            if(user.getStackInHand(hand).getDamage() < 10) {
+                world.playSound(null, user.getBlockPos(), SoundEvents.BLOCK_ANVIL_PLACE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                user.sendMessage(Text.translatable("item.terraccessories.staff_of_regrowth.fail_damage").formatted(Formatting.RED), true);
+                user.getItemCooldownManager().set(this, 20);
+                return TypedActionResult.fail(user.getStackInHand(hand));
+            }
+
+            if(user.getInventory().contains(new ItemStack(Items.BONE_MEAL))) {
+                int maxNeeded = user.getStackInHand(hand).getDamage() / 10;
+                int boneMealCount = user.getInventory().count(Items.BONE_MEAL);
+                int slot = user.getInventory().getSlotWithStack(new ItemStack(Items.BONE_MEAL));
+
+                if(boneMealCount >= 1 && (user.getStackInHand(hand).getDamage() > 10)) {
+
+                    int available = Math.min(maxNeeded, boneMealCount);
+
+                    // Repair the staff
+                    user.getStackInHand(hand).damage(-available * 10, user, (p) -> {});
+                    user.getInventory().removeStack(slot, available);
+
+                    world.playSound(null, user.getBlockPos(), SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    user.sendMessage(Text.translatable("item.terraccessories.staff_of_regrowth.success").formatted(Formatting.GREEN), true);
+
+                    return TypedActionResult.success(user.getStackInHand(hand));
+                }
+
+            }
+
+            world.playSound(null, user.getBlockPos(), SoundEvents.BLOCK_ANVIL_PLACE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            user.sendMessage(Text.translatable("item.terraccessories.staff_of_regrowth.fail_bonemeal").formatted(Formatting.RED), true);
+            user.getItemCooldownManager().set(this, 20);
+
+            return TypedActionResult.fail(user.getStackInHand(hand));
+        }
+
+        return TypedActionResult.pass(user.getStackInHand(hand));
+    }
+
+    /* Use on block */
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         if (context.getWorld().isClient) {
