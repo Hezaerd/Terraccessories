469ea6537c48d3b51ed1352b0d43fb48fb8678a1
Angel-2180 <a.gimer@student.isartdigital.com>
2024-04-07 14:06:56 -0400

    Update BootsUtils.java

--- src/main/java/net/hezaerd/terraccessories/utils/BootsUtils.java
+++ src/main/java/net/hezaerd/terraccessories/utils/BootsUtils.java
@@ -15,44 +15,54 @@ public class BootsUtils {
         GROUND,
         WATER,
         LAVA,
-        AIR
+        AIR,
+        NONE
     }
 
-
-
     public static void walkOnFluid(ItemStack stack, PlayerEntity player, Block fluid)
+    {
+        if (fluid == Blocks.WATER) {
+            walkOnWater(stack, player);
+        }
+        else if (fluid == Blocks.LAVA) {
+            walkOnLava(stack, player);
+        }
+    }
+
+    public static void walkOnWater(ItemStack stack, PlayerEntity player)
     {
         if (player == null || stack == null) return;
         NbtCompound nbt = stack.getOrCreateNbt();
         if (player.isSneaking() || player.isOnGround()) {
-            nbt.putInt("previous_state", BootsState.GROUND.ordinal());
+            nbt.putInt("previous_water_state", BootsState.GROUND.ordinal());
             player.setNoGravity(false);
             return;
         }
+
         BlockState blockStateDown = player.getWorld().getBlockState(player.getBlockPos().down());
         BlockState blockStateFoot = player.getWorld().getBlockState(player.getBlockPos());
-        //if player is previously on ground add y velocity to avoid falling on lava
-        boolean steppingOnFluid = blockStateDown.getBlock() == fluid && blockStateFoot.isAir();
-        int lavaOrWater = fluid == Blocks.LAVA ? BootsState.LAVA.ordinal() : BootsState.WATER.ordinal();
+
+        Log.i("nbt Tag: " + nbt.getInt("previous_water_state"));
+        boolean steppingOnFluid = (blockStateFoot.getBlock() == Blocks.WATER || blockStateDown.getBlock() == Blocks.WATER) && blockStateFoot.isAir();
+
         int airOrGround = blockStateDown.isAir() ? BootsState.AIR.ordinal() : BootsState.GROUND.ordinal();
-        if (nbt.getInt("previous_state") == BootsState.GROUND.ordinal() && (blockStateFoot.getBlock() == fluid || blockStateDown.getBlock() == fluid)){
+        if (nbt.getInt("previous_water_state") == BootsState.GROUND.ordinal()
+                && (blockStateFoot.getBlock() == Blocks.WATER || blockStateDown.getBlock() == Blocks.WATER)){
             player.setNoGravity(true);
-            player.setVelocity(player.getVelocity().x, 0.2, player.getVelocity().z);
-            Log.i("Setting no gravity");
+            player.setVelocity(player.getVelocity().x, 0.4, player.getVelocity().z);
             player.setNoGravity(false);
-            nbt.putInt("previous_state", lavaOrWater);
+
+            nbt.putInt("previous_water_state", BootsState.WATER.ordinal());
         }
-        else if (nbt.getInt("previous_state") == lavaOrWater && blockStateDown.getBlock() != fluid){
-            nbt.putInt("previous_state", steppingOnFluid ? lavaOrWater : airOrGround);
+        else if (nbt.getInt("previous_water_state") == BootsState.AIR.ordinal() && blockStateDown.getBlock() == Blocks.WATER){
+
+            nbt.putInt("previous_water_state", steppingOnFluid ? BootsState.WATER.ordinal() : BootsState.GROUND.ordinal());
         }
-        else if (nbt.getInt("previous_state") == BootsState.AIR.ordinal() && blockStateDown.getBlock() == fluid){
-            nbt.putInt("previous_state", steppingOnFluid ? lavaOrWater : BootsState.GROUND.ordinal());
+        else if ((nbt.getInt("previous_state") == BootsState.WATER.ordinal()
+                && blockStateDown.getBlock() != Blocks.WATER)){
+            nbt.putInt("previous_water_state", airOrGround);
         }
 
-        Log.i("Previous state: " + nbt.getInt("previous_state"));
-        Log.i("Stepping on fluid: " + steppingOnFluid);
-
-        //Get the distance to the surface of the lava
         double distanceToSurface = (Math.floor(player.getY())) - player.getY();
         if (distanceToSurface > MAX_DISTANCE_SURFACE) {
             player.setNoGravity(false);
@@ -60,15 +70,11 @@ public class BootsUtils {
         }
         if (steppingOnFluid) {
             player.setOnGround(true);
-            //Clamp the distance to the surface to a maximum of 0.04
             distanceToSurface = Math.min(0.04, distanceToSurface);
-            //Set the player's velocity to the distance to the surface if it is greater than the player's current velocity
             player.setVelocity(player.getVelocity().x, Math.max(distanceToSurface, player.getVelocity().y), player.getVelocity().z);
             MinecraftClient client = MinecraftClient.getInstance();
             if (client != null && client.player != null  && client.player.input != null)
             {
-//                client.player.setSprinting(!player.isSprinting() && client.player.input.pressingForward);
-
                 if (client.player.input.jumping){
                     player.jump();
                 }
@@ -78,6 +84,62 @@ public class BootsUtils {
             player.setOnGround(false);
         }
     }
+
+    public static void walkOnLava(ItemStack stack, PlayerEntity player)
+    {
+        if (player == null || stack == null) return;
+        NbtCompound nbt = stack.getOrCreateNbt();
+        if (player.isSneaking() || player.isOnGround()) {
+            nbt.putInt("previous_lava_state", BootsState.GROUND.ordinal());
+            player.setNoGravity(false);
+            return;
+        }
+        BlockState blockStateDown = player.getWorld().getBlockState(player.getBlockPos().down());
+        BlockState blockStateFoot = player.getWorld().getBlockState(player.getBlockPos());
+
+        Log.i("nbt Tag: " + nbt.getInt("previous_lava_state"));
+        boolean steppingOnFluid = (blockStateFoot.getBlock() == Blocks.LAVA || blockStateDown.getBlock() == Blocks.LAVA) && blockStateFoot.isAir();
+
+
+        int airOrGround = blockStateDown.isAir() ? BootsState.AIR.ordinal() : BootsState.GROUND.ordinal();
+        if (nbt.getInt("previous_lava_state") == BootsState.GROUND.ordinal() && (blockStateFoot.getBlock() == Blocks.LAVA
+                || blockStateDown.getBlock() == Blocks.LAVA)){
+            player.setNoGravity(true);
+            player.setVelocity(player.getVelocity().x, 0.4, player.getVelocity().z);
+            player.setNoGravity(false);
+
+            nbt.putInt("previous_lava_state", BootsState.LAVA.ordinal());
+        }
+        else if (nbt.getInt("previous_lava_state") == BootsState.AIR.ordinal() && blockStateDown.getBlock() == Blocks.LAVA){
+            nbt.putInt("previous_lava_state", steppingOnFluid ? BootsState.LAVA.ordinal() : BootsState.GROUND.ordinal());
+        }
+        else if ((nbt.getInt("previous_lava_state") == BootsState.LAVA.ordinal())
+                && blockStateDown.getBlock() != Blocks.LAVA){
+            nbt.putInt("previous_lava_state", airOrGround);
+        }
+
+        double distanceToSurface = (Math.floor(player.getY())) - player.getY();
+        if (distanceToSurface > MAX_DISTANCE_SURFACE) {
+            player.setNoGravity(false);
+            return;
+        }
+        if (steppingOnFluid) {
+            player.setOnGround(true);
+            distanceToSurface = Math.min(0.04, distanceToSurface);
+            player.setVelocity(player.getVelocity().x, Math.max(distanceToSurface, player.getVelocity().y), player.getVelocity().z);
+            MinecraftClient client = MinecraftClient.getInstance();
+            if (client != null && client.player != null  && client.player.input != null)
+            {
+                if (client.player.input.jumping){
+                    player.jump();
+                }
+            }
+        }
+        else {
+            player.setOnGround(false);
+        }
+    }
+
 
 
 }
